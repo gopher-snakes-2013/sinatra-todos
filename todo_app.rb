0257911559b80b7b2ead0e777512411f852d6dee
Zee Spencer <zee@zeespencer.com>
2013-10-08 11:05:13 -0700

    Bugfix: Prevent Heroku from trying to load dotenv
    * Wrap require 'dotenv' in a begin rescue end

--- todo_app.rb
+++ todo_app.rb
@@ -6,9 +6,12 @@ require 'sinatra/activerecord'
 require 'models/todo'
 
 
-# This loads environment variables from the .env file
-require 'dotenv'
-Dotenv.load
+begin
+  # This loads environment variables from the .env file
+  require 'dotenv'
+  Dotenv.load
+rescue LoadError
+end
 
 set :database, ENV['DATABASE_URL']
 
